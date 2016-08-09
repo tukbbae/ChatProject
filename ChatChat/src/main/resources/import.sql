8104fd3c95878aed8067c2d1cd01bdfdf2be581b
tukbbae <tukbbae@gmail.com>
2016-08-09 22:30:22 +0900

    sql commit

--- ChatChat/src/main/resources/import.sql
+++ ChatChat/src/main/resources/import.sql
@@ -7,6 +7,4 @@ INSERT INTO account(username, email, activate_date, is_password_changed, last_lo
 -- User
 -- =================================================================================================
 INSERT INTO user(email, name, token, device_type, updated_date) VALUES ('tukbbae@gmail.com', '이현민', 'e2S9zTlQL6Y:APA91bHVjB-nsRZ7-npxeCYDbedX15fdXsTQEPCqz313tT2SXB-N8XwKRMSkVbndCah9EOZaB6dD0GuyfDRsyLSWbo0p1YXEkyt4FHvwRZV6B2DjWZ2kFhvSDvArUde9aeg-kjFHW2HD', 'A', '2016-08-09');
-INSERT INTO user(email, name, token, device_type, updated_date) VALUES ('user1@gmail.com', '유저1', 'e2S9zTlQL6Y:APA91bHVjB-nsRZ7-npxeCYDbedX15fdXsTQEPCqz313tT2SXB-N8XwKRMSkVbndCah9EOZaB6dD0GuyfDRsyLSWbo0p1YXEkyt4FHvwRZV6B2DjWZ2kFhvSDvArUde9aeg-kjFHW2HD', 'A', '2016-08-09');
-INSERT INTO user(email, name, token, device_type, updated_date) VALUES ('user2@gmail.com', '유저2', 'e2S9zTlQL6Y:APA91bHVjB-nsRZ7-npxeCYDbedX15fdXsTQEPCqz313tT2SXB-N8XwKRMSkVbndCah9EOZaB6dD0GuyfDRsyLSWbo0p1YXEkyt4FHvwRZV6B2DjWZ2kFhvSDvArUde9aeg-kjFHW2HD', 'A', '2016-08-09');
 
