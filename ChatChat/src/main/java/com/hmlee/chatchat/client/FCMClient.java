60febcae66cca8596e765b1e6630f1d323d96a72
tukbbae <tukbbae@gmail.com>
2016-08-18 23:11:19 +0900

    API Key 변경

--- ChatChat/src/main/java/com/hmlee/chatchat/client/FCMClient.java
+++ ChatChat/src/main/java/com/hmlee/chatchat/client/FCMClient.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
 public class FCMClient {
 
 	// Firebase FCM API Key
-	private final static String API_KEY = "AIzaSyCEjH-fwSbgKqnZg0qaSHFTq9hGDAgPM1k";
+	private final static String API_KEY = "AIzaSyBh5oxRvItWrfLVF9e0noRTMFOpiZ6llyk";
 	private static FCMClient fcmClient = null;
 
 	private static FcmRequestBody request;
